35c2e6be7231ed714e7eceb8cdb61faff8234028
Ajit Singh <asingh0525@gmail.com>
2024-02-29 14:54:54 -0500

    added starter code

--- mp1/mp1.c
+++ mp1/mp1.c
@@ -9,6 +9,27 @@ MODULE_AUTHOR("Your_NetID");
 MODULE_DESCRIPTION("CS-423 MP1");
 
 #define DEBUG 1
+#define FILENAME "status"
+#define DIRECTORY "mp1"
+
+static struct proc_dir_entry *proc_dir;
+static struct proc_dir_entry *proc_entry;
+
+// read
+static ssize_t mp1_read(struct file *file, char __user *buffer, size_t count, loff_t *data) {
+
+}
+
+// write
+static ssize_t mp1_write(struct file *file, const char __user *buffer, size_t count, loff_t *data) {
+
+}
+
+static const struct file_operations mp1_file = {
+   .owner = THIS_MODULE,
+   .read = mp1_read,
+   .write = mp1_write,
+};
 
 // mp1_init - Called when module is loaded
 int __init mp1_init(void)
@@ -17,8 +38,10 @@ int __init mp1_init(void)
    printk(KERN_ALERT "MP1 MODULE LOADING\n");
    #endif
    // Insert your code here ...
-   
-   
+
+   // create direc and entry within proc filesys
+   proc_dir = proc_mkdir(DIRECTORY, NULL);
+   proc_entry = proc_create(FILENAME, 0666, proc_dir, & mp1_file);
    
    printk(KERN_ALERT "MP1 MODULE LOADED\n");
    return 0;   
