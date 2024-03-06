26989edbdb5c921a8329be1e9b07e1178fec85bf
Ajit Singh <asingh0525@gmail.com>
2024-03-06 11:35:36 -0500

    finished step 9

--- mp1/userapp.c
+++ mp1/userapp.c
@@ -2,6 +2,7 @@
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/types.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
@@ -15,5 +16,15 @@ int main(int argc, char* argv[])
 	fprintf(fptr, "%d", pid);
 	fclose(fptr);
 
+	// sleep 15 sec
+	printf("start sleep \n");
+	sleep(15);
+	printf("done sleep \n");
+
+	// FILE *fp = popen(command, "w");
+	// fprintf(fp, "cat /proc/mp1/status");
+	// fclose(fp);
+	
+	system("cat /proc/mp1/status");
 	return 0;
 }
